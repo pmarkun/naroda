9acb8967c27ff7300915064f39d90ee78c507680
Pedro Markun <pedro@markun.com.br>
2026-05-22 23:19:09 -0300

    Add PWA infrastructure: manifest, service worker, icons

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE = 'naroda-v1';
+
+const URLS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './questions.json',
+  './manifest.json',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (k) { return k !== CACHE; })
+          .map(function (k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  var requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request);
+    })
+  );
+});
